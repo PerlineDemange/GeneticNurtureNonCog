89a6e857eb7b7eafbf88c6e3bc9b69bd277e9139
Perline Demange <p.a.d.demange@vu.nl>
2020-03-31 18:56:02 +0200

    add analysis with bootstrap, 1000 resampling

--- UKB/Siblings/siblings_UKB.R
+++ UKB/Siblings/siblings_UKB.R
@@ -191,3 +191,129 @@ ratio_Cog <- indirect_Cog/direct_Cog #1.278479
 #####################
 write.table(finalsib, file="Data_scores_siblings_UKB_20200310.csv", row.names=F, quote=F) 
 
+
+
+#################
+# Bootstrap 
+#################
+
+library(boot)
+nboot <- 1000
+bootcoef<-function(data,index){
+  datx<-finalsib[index,]
+  mod<-lme(EA_sc~GPS_B_NonCog + GPS_B_Cog + GPS_W_NonCog + GPS_W_Cog + sex + array + Age + sex*Age + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10, random=~1|FID, method="ML", na.action=na.omit, data=datx,control=lmeControl(opt = "optim"))
+  fixef(mod) #get fixed effects
+}
+
+# carry out bootstrap
+boot.out<-boot(finalsib,bootcoef,nboot) #parallel = "multicore", ncpus=20
+
+#plot to check bootstrapping
+png("UKB.sib.bootstrap.png",
+    width = 10,
+    height = 6,
+    units = 'in',
+    res = 600)
+plot(boot.out)
+dev.off()
+
+# get CI 
+#boot.ci(boot.out)
+#Error in bca.ci(boot.out, conf, index[1L], L = L, t = t.o, t0 = t0.o,  :
+#                  estimated adjustment 'a' is NA
+                
+boot.ci(boot.out, type = c("norm", "basic"))
+
+#Intervals :
+#  Level      Normal              Basic
+#95%   ( 1.025,  1.231 )   ( 1.014,  1.228 )
+
+
+# save results for each bootstrap in data frame
+bootoutput <- as.data.frame(boot.out$t)
+colnames(bootouput) <- rownames(as.data.frame(boot.out$t0))
+write.table(bootoutput, "Data_scores_siblings_UKB_bootstrapped_20203031.csv", row.names=F, quote=F)
+bmain <- bootoutput[,2:5] #Same variables of interest
+
+# Create indirect and ratio variables
+bmain$indirect_NonCog <- bmain$GPS_B_NonCog - bmain$GPS_W_NonCog
+bmain$indirect_Cog <- bmain$GPS_B_Cog - bmain$GPS_W_Cog
+bmain$ratio_NonCog <- bmain$indirect_NonCog / bmain$GPS_W_NonCog
+bmain$ratio_Cog <- bmain$indirect_Cog / bmain$GPS_W_Cog
+write.table(bmain, "Data_scores_siblings_UKB_bootstrapped_ratio_20203031.csv", row.names=F, quote=F)
+
+# perform t.tests
+
+t.test(bmain$indirect_NonCog, bmain$GPS_W_NonCog)
+# Welch Two Sample t-test
+# 
+# data:  bmain$indirect_NonCog and bmain$GPS_W_NonCog
+# t = 110.48, df = 1980.3, p-value < 2.2e-16
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.04669162 0.04837918
+# sample estimates:
+#   mean of x mean of y
+# 0.1574980 0.1099626
+
+t.test(bmain$indirect_Cog, bmain$GPS_W_Cog)
+# Welch Two Sample t-test
+# 
+# data:  bmain$indirect_Cog and bmain$GPS_W_Cog
+# t = 79.3, df = 1983.7, p-value < 2.2e-16
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.0354996 0.0373000
+# sample estimates:
+#   mean of x mean of y
+# 0.1588555 0.1224557
+
+t.test(bmain$indirect_NonCog, bmain$indirect_Cog)
+# Welch Two Sample t-test
+# 
+# data:  bmain$indirect_NonCog and bmain$indirect_Cog
+# t = -2.9237, df = 1990.8, p-value = 0.003498
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -0.0022681418 -0.0004469377
+# sample estimates:
+#   mean of x mean of y
+# 0.1574980 0.1588555
+
+t.test(bmain$ratio_NonCog, bmain$ratio_Cog)
+# Welch Two Sample t-test
+# 
+# data:  bmain$ratio_NonCog and bmain$ratio_Cog
+# t = 15.248, df = 1983, p-value < 2.2e-16
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   0.1193758 0.1546165
+# sample estimates:
+#   mean of x mean of y
+# 1.449258  1.312262
+
+
+# Get CI of all 
+meanall <- apply(bmain, 2, mean)
+sdall <- apply(bmain, 2, sd)
+n <- nboot
+error <- qnorm(0.975)*sdall/sqrt(n)
+leftCI <- meanall-error
+rightCI <- meanall+error
+
+tot <- rbind(meanall, sdall, error, leftCI, rightCI)
+# GPS_B_NonCog    GPS_B_Cog GPS_W_NonCog    GPS_W_Cog indirect_NonCog
+# meanall 0.2674605122 0.2813111948 0.1099625552 0.1224556980    0.1574979571
+# sdall   0.0044916333 0.0044946202 0.0091543089 0.0098188458    0.0100652506
+# error   0.0002783892 0.0002785743 0.0005673795 0.0006085672    0.0006238392
+# leftCI  0.2671821230 0.2810326205 0.1093951757 0.1218471308    0.1568741178
+# rightCI 0.2677389014 0.2815897692 0.1105299347 0.1230642652    0.1581217963
+# indirect_Cog ratio_NonCog  ratio_Cog
+# meanall 0.1588554968   1.44925845 1.31226233
+# sdall   0.0106903139   0.20945458 0.19197017
+# error   0.0006625803   0.01298189 0.01189822
+# leftCI  0.1581929165   1.43627656 1.30036411
+# rightCI 0.1595180772   1.46224035 1.32416055
+
+write.table(tot, "summary_mean_CI_siblings_UKB_20200331.csv", row.names=T, quote=F)
+
